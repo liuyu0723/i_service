75580829ae9f1922fc70355e30585b4f8a0aa30c
albert <liuyu0723@gmail.com>
2019-09-04 11:09:31 +1200

    fixed default value not match issues
    fixed default value not match issues

--- application/library/convertor/GroupActivity.php
+++ application/library/convertor/GroupActivity.php
@@ -3,9 +3,11 @@
 /**
  * 活动结果转换器类
  */
-class Convertor_GroupActivity extends Convertor_Base {
+class Convertor_GroupActivity extends Convertor_Base
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
@@ -22,38 +24,34 @@ class Convertor_GroupActivity extends Convertor_Base {
      *            扩展参数
      * @return multitype:multitype: array
      */
-    public function getEffectiveActivityListConvertor($activityList, $tagList, $activityCount, $param) {
+    public function getEffectiveActivityListConvertor($activityList, $tagList, $activityCount, $param)
+    {
         $tagListNew = array();
         foreach ($tagList as $tag) {
-            $tagListNew [$tag ['id']] = $this->handlerMultiLang('title', $tag);
+            $tagListNew[$tag['id']] = $this->handlerMultiLang('title', $tag);
         }
         $data = array('list' => array());
         foreach ($activityList as $activity) {
             $activityTemp = array();
-            $activityTemp ['id'] = $activity ['id'];
-            $activityTemp ['title'] = $activity ['title'];
-            $activityTemp ['article'] = Enum_Img::getPathByKeyAndType($activity ['article']);
-            $activityTemp ['pdf'] = $activity['pdf'] ? Enum_Img::getPathByKeyAndType($activity['pdf']) : '';
-            $activityTemp ['video'] = $activity['video'] ? Enum_Img::getPathByKeyAndType($activity['video']) : '';
-            $activityTemp ['tagId'] = $activity ['tagid'];
-            $activityTemp ['pic'] = $activity ['pic'];
-            $activityTemp ['fromdate'] = $activity ['fromdate'];
-            $activityTemp ['todate'] = $activity ['todate'];
-            $activityTemp ['ordercount'] = $activity ['ordercount'];
-            $activityTemp ['tagName'] = $tagListNew [$activityTemp ['tagId']];
-            $activityTemp ['createtime'] = $activity ['createtime'];
-            $activityTemp ['updatetime'] = $activity ['updatetime'];
-            $activityTemp ['pic'] = Enum_Img::getPathByKeyAndType($activity ['pic'], Enum_Img::PIC_TYPE_KEY_WIDTH750);
-            $activityTemp ['pdf'] = Enum_Img::getPathByKeyAndType($activity ['pdf']);
-            $activityTemp ['video'] = Enum_Img::getPathByKeyAndType($activity ['video']);
-            $activityTemp ['fromdate'] = $activity ['fromdate'];
-            $activityTemp ['todate'] = $activity ['todate'];
-            $data ['list'] [] = $activityTemp;
+            $activityTemp['id'] = $activity['id'];
+            $activityTemp['title'] = $activity['title'];
+            $activityTemp['article'] = Enum_Img::getPathByKeyAndType($activity['article']);
+            $activityTemp['pdf'] = $activity['pdf'] ? Enum_Img::getPathByKeyAndType($activity['pdf']) : '';
+            $activityTemp['video'] = $activity['video'] ? Enum_Img::getPathByKeyAndType($activity['video']) : '';
+            $activityTemp['tagId'] = $activity['tagid'];
+            $activityTemp['pic'] = $activity['pic'];
+            $activityTemp['fromdate'] = $activity['fromdate'];
+            $activityTemp['todate'] = $activity['todate'];
+            $activityTemp['ordercount'] = $activity['ordercount'];
+            $activityTemp['tagName'] = $tagListNew[$activityTemp['tagId']];
+            $activityTemp['createtime'] = $activity['createtime'];
+            $activityTemp['updatetime'] = $activity['updatetime'];
+            $data['list'][] = $activityTemp;
         }
-        $data ['total'] = $activityCount;
-        $data ['page'] = $param ['page'];
-        $data ['limit'] = $param ['limit'];
-        $data ['nextPage'] = Util_Tools::getNextPage($data ['page'], $data ['limit'], $data ['total']);
+        $data['total'] = $activityCount;
+        $data['page'] = $param['page'];
+        $data['limit'] = $param['limit'];
+        $data['nextPage'] = Util_Tools::getNextPage($data['page'], $data['limit'], $data['total']);
         return $data;
     }
 
@@ -68,47 +66,48 @@ class Convertor_GroupActivity extends Convertor_Base {
      *            扩展参数
      * @return multitype:multitype: array
      */
-    public function getActivityListConvertor($list, $count, $param) {
+    public function getActivityListConvertor($list, $count, $param)
+    {
         $data = array('list' => array());
         $groupIdList = array_column($list, 'groupid');
-        $groupModel = new GroupModel ();
+        $groupModel = new GroupModel();
         $groupInfoList = $groupModel->getGroupList(array('id' => $groupIdList));
         $groupNameList = array_column($groupInfoList, 'name', 'id');
         $tagidList = array_column($list, 'tagid');
         if ($tagidList) {
-            $activityTagModel = new GroupActivityTagModel ();
+            $activityTagModel = new GroupActivityTagModel();
             $activityTagList = $activityTagModel->getActivityTagList(array('id' => $tagidList));
             $activityTagNameList = array_column($activityTagList, 'title', 'id');
         }
         foreach ($list as $key => $value) {
             $activityTemp = array();
-            $activityTemp ['id'] = $value ['id'];
-            $activityTemp ['title'] = $value ['title'];
-            $activityTemp ['article'] = $value ['article'];
-            $activityTemp ['pic'] = $value ['pic'];
-            $activityTemp ['fromdate'] = $value ['fromdate'];
-            $activityTemp ['todate'] = $value ['todate'];
-            $activityTemp ['ordercount'] = $value ['ordercount'];
-            $activityTemp ['groupid'] = $value ['groupid'];
-            $activityTemp ['groupName'] = $groupNameList [$activityTemp ['groupid']];
-            $activityTemp ['status'] = $value ['status'];
-            $activityTemp ['createtime'] = $value ['createtime'];
-            $activityTemp ['updatetime'] = $value ['updatetime'];
-            $activityTemp ['tagid'] = $value ['tagid'];
-            $activityTemp ['tagName'] = $activityTagNameList [$activityTemp ['tagid']];
-            $activityTemp ['sort'] = $value ['sort'];
-            $activityTemp ['pdf'] = $value ['pdf'];
-            $activityTemp ['video'] = $value ['video'];
-            $activityTemp ['pic'] = $value ['pic'];
-            $activityTemp ['fromdate'] = $value ['fromdate'];
-            $activityTemp ['todate'] = $value ['todate'];
-            $activityTemp ['count'] = $value ['count'];
-            $data ['list'] [] = $activityTemp;
+            $activityTemp['id'] = $value['id'];
+            $activityTemp['title'] = $value['title'];
+            $activityTemp['article'] = $value['article'];
+            $activityTemp['pic'] = $value['pic'];
+            $activityTemp['fromdate'] = $value['fromdate'];
+            $activityTemp['todate'] = $value['todate'];
+            $activityTemp['ordercount'] = $value['ordercount'];
+            $activityTemp['groupid'] = $value['groupid'];
+            $activityTemp['groupName'] = $groupNameList[$activityTemp['groupid']];
+            $activityTemp['status'] = $value['status'];
+            $activityTemp['createtime'] = $value['createtime'];
+            $activityTemp['updatetime'] = $value['updatetime'];
+            $activityTemp['tagid'] = $value['tagid'];
+            $activityTemp['tagName'] = $activityTagNameList[$activityTemp['tagid']];
+            $activityTemp['sort'] = $value['sort'];
+            $activityTemp['pdf'] = $value['pdf'];
+            $activityTemp['video'] = $value['video'];
+            $activityTemp['pic'] = $value['pic'];
+            $activityTemp['fromdate'] = $value['fromdate'];
+            $activityTemp['todate'] = $value['todate'];
+            $activityTemp['count'] = $value['count'];
+            $data['list'][] = $activityTemp;
         }
-        $data ['total'] = $count;
-        $data ['page'] = $param ['page'];
-        $data ['limit'] = $param ['limit'];
-        $data ['nextPage'] = Util_Tools::getNextPage($data ['page'], $data ['limit'], $data ['total']);
+        $data['total'] = $count;
+        $data['page'] = $param['page'];
+        $data['limit'] = $param['limit'];
+        $data['nextPage'] = Util_Tools::getNextPage($data['page'], $data['limit'], $data['total']);
         return $data;
     }
 }
--- application/models/Poi.php
+++ application/models/Poi.php
@@ -4,13 +4,15 @@
  * Class PoiModel
  * 本地攻略管理
  */
-class PoiModel extends \BaseModel {
+class PoiModel extends \BaseModel
+{
 
     private $dao;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
-        $this->dao = new Dao_Poi ();
+        $this->dao = new Dao_Poi();
     }
 
     /**
@@ -20,15 +22,16 @@ class PoiModel extends \BaseModel {
      *            array param 查询条件
      * @return array
      */
-    public function getPoiList(array $param) {
-        $param ['typeid'] ? $paramList ['typeid'] = $param ['typeid'] : false;
-        $param ['tagid'] ? $paramList ['tagid'] = $param ['tagid'] : false;
-        $param ['id'] ? $paramList ['id'] = $param ['id'] : false;
-        $param ['name'] ? $paramList ['name'] = $param ['name'] : false;
-        isset ($param ['hotelid']) ? $paramList ['hotelid'] = $param ['hotelid'] : false;
-        isset ($param ['status']) ? $paramList ['status'] = $param ['status'] : false;
-        $paramList ['limit'] = $param ['limit'];
-        $paramList ['page'] = $param ['page'];
+    public function getPoiList(array $param)
+    {
+        $param['typeid'] ? $paramList['typeid'] = $param['typeid'] : false;
+        $param['tagid'] ? $paramList['tagid'] = $param['tagid'] : false;
+        $param['id'] ? $paramList['id'] = $param['id'] : false;
+        $param['name'] ? $paramList['name'] = $param['name'] : false;
+        isset($param['hotelid']) ? $paramList['hotelid'] = $param['hotelid'] : false;
+        isset($param['status']) ? $paramList['status'] = $param['status'] : false;
+        $paramList['limit'] = $param['limit'];
+        $paramList['page'] = $param['page'];
         return $this->dao->getPoiList($paramList);
     }
 
@@ -39,13 +42,14 @@ class PoiModel extends \BaseModel {
      *            array param 查询条件
      * @return array
      */
-    public function getPoiCount(array $param) {
-        $param ['typeid'] ? $paramList ['typeid'] = $param ['typeid'] : false;
-        $param ['tagid'] ? $paramList ['tagid'] = $param ['tagid'] : false;
-        $param ['id'] ? $paramList ['id'] = $param ['id'] : false;
-        $param ['name'] ? $paramList ['name'] = $param ['name'] : false;
-        isset ($param ['hotelid']) ? $paramList ['hotelid'] = $param ['hotelid'] : false;
-        isset ($param ['status']) ? $paramList ['status'] = $param ['status'] : false;
+    public function getPoiCount(array $param)
+    {
+        $param['typeid'] ? $paramList['typeid'] = $param['typeid'] : false;
+        $param['tagid'] ? $paramList['tagid'] = $param['tagid'] : false;
+        $param['id'] ? $paramList['id'] = $param['id'] : false;
+        $param['name'] ? $paramList['name'] = $param['name'] : false;
+        isset($param['hotelid']) ? $paramList['hotelid'] = $param['hotelid'] : false;
+        isset($param['status']) ? $paramList['status'] = $param['status'] : false;
         return $this->dao->getPoiCount($paramList);
     }
 
@@ -56,7 +60,8 @@ class PoiModel extends \BaseModel {
      *            int id 查询的主键
      * @return array
      */
-    public function getPoiDetail($id) {
+    public function getPoiDetail($id)
+    {
         $result = array();
         if ($id) {
             $result = $this->dao->getPoiDetail($id);
@@ -73,34 +78,48 @@ class PoiModel extends \BaseModel {
      *            int id 主键
      * @return array
      */
-    public function updatePoiById($param, $id) {
+    public function updatePoiById($param, $id)
+    {
         $result = false;
         if ($id) {
             $info = array();
-            isset ($param ['hotelid']) ? $info ['hotelid'] = intval($param ['hotelid']) : false;
-            isset ($param ['typeid']) ? $info ['typeid'] = intval($param ['typeid']) : false;
-            isset ($param ['tagid']) ? $info ['tagid'] = intval($param ['tagid']) : false;
-            isset ($param ['name_lang1']) ? $info ['name_lang1'] = $param ['name_lang1'] : false;
-            isset ($param ['name_lang2']) ? $info ['name_lang2'] = $param ['name_lang2'] : false;
-            isset ($param ['name_lang3']) ? $info ['name_lang3'] = $param ['name_lang3'] : false;
-            isset ($param ['detail_lang1']) ? $info ['detail_lang1'] = $param ['detail_lang1'] : false;
-            isset ($param ['detail_lang2']) ? $info ['detail_lang2'] = $param ['detail_lang2'] : false;
-            isset ($param ['detail_lang3']) ? $info ['detail_lang3'] = $param ['detail_lang3'] : false;
-            isset ($param ['address_lang1']) ? $info ['address_lang1'] = $param ['address_lang1'] : false;
-            isset ($param ['address_lang2']) ? $info ['address_lang2'] = $param ['address_lang2'] : false;
-            isset ($param ['address_lang3']) ? $info ['address_lang3'] = $param ['address_lang3'] : false;
-            isset ($param ['introduct_lang1']) ? $info ['introduct_lang1'] = $param ['introduct_lang1'] : false;
-            isset ($param ['introduct_lang2']) ? $info ['introduct_lang2'] = $param ['introduct_lang2'] : false;
-            isset ($param ['introduct_lang3']) ? $info ['introduct_lang3'] = $param ['introduct_lang3'] : false;
-            isset ($param ['tel']) ? $info ['tel'] = $param ['tel'] : false;
-            isset ($param ['lat']) ? $info ['lat'] = $param ['lat'] : false;
-            isset ($param ['lng']) ? $info ['lng'] = $param ['lng'] : false;
-            isset ($param ['status']) ? $info ['status'] = intval($param ['status']) : false;
+            isset($param['hotelid']) ? $info['hotelid'] = intval($param['hotelid']) : false;
+            isset($param['typeid']) ? $info['typeid'] = intval($param['typeid']) : false;
+            isset($param['tagid']) ? $info['tagid'] = intval($param['tagid']) : false;
+            isset($param['name_lang1']) ? $info['name_lang1'] = $param['name_lang1'] : false;
+            isset($param['name_lang2']) ? $info['name_lang2'] = $param['name_lang2'] : false;
+            isset($param['name_lang3']) ? $info['name_lang3'] = $param['name_lang3'] : false;
+            isset($param['detail_lang1']) ? $info['detail_lang1'] = $param['detail_lang1'] : false;
+            isset($param['detail_lang2']) ? $info['detail_lang2'] = $param['detail_lang2'] : false;
+            isset($param['detail_lang3']) ? $info['detail_lang3'] = $param['detail_lang3'] : false;
+            isset($param['address_lang1']) ? $info['address_lang1'] = $param['address_lang1'] : false;
+            isset($param['address_lang2']) ? $info['address_lang2'] = $param['address_lang2'] : false;
+            isset($param['address_lang3']) ? $info['address_lang3'] = $param['address_lang3'] : false;
+            isset($param['introduct_lang1']) ? $info['introduct_lang1'] = $param['introduct_lang1'] : false;
+            isset($param['introduct_lang2']) ? $info['introduct_lang2'] = $param['introduct_lang2'] : false;
+            isset($param['introduct_lang3']) ? $info['introduct_lang3'] = $param['introduct_lang3'] : false;
+            isset($param['tel']) ? $info['tel'] = $param['tel'] : false;
+            isset($param['lat']) ? $info['lat'] = $param['lat'] : false;
+            isset($param['lng']) ? $info['lng'] = $param['lng'] : false;
+            isset($param['status']) ? $info['status'] = intval($param['status']) : false;
             isset($param['sort']) ? $info['sort'] = $param['sort'] : false;
             isset($param['pdf']) ? $info['pdf'] = $param['pdf'] : false;
             isset($param['video']) ? $info['video'] = $param['video'] : false;
             isset($param['pic']) ? $info['pic'] = $param['pic'] : false;
-            $info ['updatetime'] = time();
+
+            isset($param['homeShow']) ? $info['homeShow'] = $param['homeShow'] : 0;
+            if ($info['homeShow'] == '') {
+                $info['homeShow'] = 1;
+            }
+            isset($param['startTime']) ? $info['startTime'] = $param['startTime'] : time();
+            if ($info['startTime'] == '') {
+                $info['startTime'] = time();
+            }
+            isset($param['endTime']) ? $info['endTime'] = $param['endTime'] : time();
+            if ($info['endTime'] == '') {
+                $info['endTime'] = time();
+            }
+            $info['updatetime'] = time();
             $result = $this->dao->updatePoiById($info, $id);
         }
         return $result;
@@ -113,8 +132,21 @@ class PoiModel extends \BaseModel {
      *            array param 需要增加的信息
      * @return array
      */
-    public function addPoi($param) {
+    public function addPoi($param)
+    {
         $info = $param;
         return $this->dao->addPoi($info);
+    }
+
+    /**
+     * 获取首页广告列表
+     *
+     * @return Json
+     */
+    public function getHomeAdv(array $param)
+    {
+        isset($param['hotelid']) ? $paramList['hotelid'] = $param['hotelid'] : false;
+        $paramList['today'] = $param['today'];
+        return $this->dao->getHomeAdv($paramList);
     }
 }
